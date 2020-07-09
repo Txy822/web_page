bfc762cdd2ed7802574c5d04acd161667de9fd8f
Txy822 <tyohanis2018@gmail.com>
2020-07-09 13:07:44 +0100

    cv_section updated

--- cv/tests.py
+++ cv/tests.py
@@ -1,23 +1,23 @@
 from django.test import TestCase
 
 # Create your tests here.
-from .models import Cv
+from .models import Cv_section
 
 
 # Another Test: Entrys
 #
 # Did you notice that the pluralization of cv is misspelled in the admin interface? “cvs” should instead read “cvies”.
 # Let’s write a test to verify that when Django correctly pluralizes “cv” to “cvies”.
-class CvModelTest(TestCase):
+class cv_section_model_test(TestCase):
     def test_title_str_representation(self):
-        tit= Cv(title="mytitle")
+        tit= Cv_section(title="mytitle")
         self.assertEqual(str(tit),tit.title)
     # def test_text_representation(self):
     #     tex= Cv(text="this is my text")
     #     self.assertEqual("this is my text",tex.text)
     def test_verbose_name_plural(self):
         #self.assertEqual(str(Cv._meta.verbose_name_plural), "cvs")
-        self.assertEqual(str(Cv._meta.verbose_name_plural), "cvies")
+        self.assertEqual(str(Cv_section._meta.verbose_name_plural), "Cv_sections")
 
 #         Every site should have a homepage. Let’s write a failing test for that.
 #
