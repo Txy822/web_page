79827aa35681fdffb19a6f578eba5230bbfee1bc
Txy822 <tyohanis2018@gmail.com>
2020-07-11 12:57:03 +0100

    functional test added

--- cv/tests/test_urls.py
+++ cv/tests/test_urls.py
@@ -6,18 +6,23 @@ from cv.models import Cv_section
 
 
 class TestUrls(TestCase):
+
     def test_cv_section_list_url_is_resolved(self):
         url=reverse('cv_section_list')
         self.assertEqual(resolve(url).func,cv_section_list)
+
     def test_cv_section_detail_url_resolveded(self):
         url=reverse('cv_section_detail',args=[1])
         self.assertEqual(resolve(url).func,cv_section_detail)
+
     def test_cv_section_new_url_resolveded(self):
         url=reverse('cv_section_new')
         self.assertEqual(resolve(url).func,cv_section_new)
+
     def test_cv_section_edit_url_resolveded(self):
         url=reverse('cv_section_edit',args=[1])
         self.assertEqual(resolve(url).func,cv_section_edit)
+
     def test_cv_section_remove_url_resolveded(self):
         url=reverse('cv_section_remove',args=[1])
         self.assertEqual(resolve(url).func,cv_section_remove)
