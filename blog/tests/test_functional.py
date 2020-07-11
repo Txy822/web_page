1f2b9062c6028233638fc1611a408416886066e1
Txy822 <tyohanis2018@gmail.com>
2020-07-11 12:53:57 +0100

    functional test added

--- blog/tests/test_functional.py
+++ blog/tests/test_functional.py
@@ -6,9 +6,30 @@ class FunctionalTestCase(TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+
     def test_there_is_homepage(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('Personal Blogs',self.browser.page_source)
+
+    def test_hompage_title_exist(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('Tesfahun blog', self.browser.title)
+        #self.fail('Finish the test!')
+
+        # She is invited to enter a to-do item straight away
+
+    def test_there_is_cv_element_from_homepage(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Curriculum Vitae(CV)',self.browser.page_source)
+
+    def test_there_is_home_element_from_homepage(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Home',self.browser.page_source)
+
     def tearDown(self):
         self.browser.quit()
 
---
+++ cv/tests/test_functional.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+from selenium import webdriver
+import unittest
+
+class FunctionalTestCase(TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def test_there_is_cv_page(self):
+        self.browser.get('http://localhost:8000/cv')
+        self.assertIn('Curriculum Vitae(CV)',self.browser.page_source)
+
+    def test_cv_page_title_exist(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000/cv')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('Tesfahun Curriculum Vitae', self.browser.title)
+        #self.fail('Finish the test!')
+
+        # She is invited to enter a to-do item straight away
+
+    def tearDown(self):
+        self.browser.quit()
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
