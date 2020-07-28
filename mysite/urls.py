19a64c586a8f7f2627a01510d863f331c31112bb
Txy822 <tyohanis2018@gmail.com>
2020-07-28 21:18:47 +0100

    basics of home page added

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('cv/',include('cv.urls'))
 ]
