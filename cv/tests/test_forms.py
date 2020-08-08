393fec5a898a27606207aa5a4c59b0902193be4b
Txy822 <tyohanis2018@gmail.com>
2020-08-08 16:04:24 +0100

    cv form test added and functional test edited

--- cv/tests/test_forms.py
+++ cv/tests/test_forms.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from cv.models import Cv_section
+from cv.forms import Cv_section_form
 
 class CvSectionFormTest(TestCase):
 
@@ -9,4 +10,14 @@ class CvSectionFormTest(TestCase):
         pass
     def test_form(self):
         pass
-        
+    def test_cv_section_form_valid_data(self):
+        form =Cv_section_form(data={
+        'title' : 'Form Title 123',
+        'text' : 'this is my form text'
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_cv_section_form_no_data(self):
+        form=Cv_section_form(data={})
+        self.assertFalse(form.is_valid())
+        self.assertEquals(len(form.errors),2)
